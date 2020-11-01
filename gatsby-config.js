1db98df41fabd7e73df0d8da52cc2e644c3e782f
Damian Kalka <kalka762@gmail.com>
2020-11-01 17:08:23 +0100

    add styled-components and prop-types to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,5 +63,17 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`400`, `600`, `700`, `800`, `900`],
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-layout`,
   ],
 };
