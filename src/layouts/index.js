fbbe26c73243a607f385232001b0918fe9b6c541
Damian Kalka <kalka762@gmail.com>
2020-11-01 17:39:43 +0100

    add global theme variables

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
-// import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import propTypes from 'prop-types';
 import GlobalStyle from '../GlobalStyles/GlobalStyles';
+import { breakpoints, colors, fonts, media, paddings } from '../theme/theme';
 
 const MainLayout = ({ children }) => {
   return (
-    <>
+    <ThemeProvider
+      breakpoints={breakpoints}
+      padddings={paddings}
+      fonts={fonts}
+      colors={colors}
+      media={media}
+    >
       <GlobalStyle />
       <h1>Hello World!</h1>
       {children}
-    </>
+    </ThemeProvider>
   );
 };
 
 MainLayout.propTypes = {
-  children: propTypes.element.isRequired,
+  children: propTypes.element,
+};
+
+MainLayout.defaultProps = {
+  children: <></>,
 };
 
 export default MainLayout;
---
+++ src/theme/theme.js
@@ -0,0 +1,34 @@
+export const breakpoints = {
+  desktop: '1920px',
+  laptop: '1366px',
+  tablet: '1024px',
+  mobile: '375px',
+};
+
+export const paddings = {
+  desktop: '60px',
+  laptop: '60px',
+  tablet: '60px',
+  mobile: '10px',
+};
+
+export const fonts = {
+  regular: 400,
+  semiBold: 600,
+  Bold: 700,
+  ExtraBold: 800,
+  Black: 900,
+};
+
+export const colors = {
+  mainBlue: '#61DAFB',
+  mainFair: '#F1EAEA',
+  mainDark: '#372f2f',
+};
+
+export const media = {
+  desktop: `(min-width: ${breakpoints.desktop})`,
+  laptop: `(min-width: ${breakpoints.desktop})`,
+  tablet: `(min-width: ${breakpoints.desktop})`,
+  phone: `(min-width: ${breakpoints.desktop})`,
+};
