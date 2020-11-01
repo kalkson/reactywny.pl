e054be169ac09dbb1907f2842291d5e90e4363f8
Damian Kalka <kalka762@gmail.com>
2020-11-01 16:32:14 +0100

    install config dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,5 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-const IndexPage = () => (
-  <>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </>
-)
+const IndexPage = () => <>Hello</>;
 
-export default IndexPage
+export default IndexPage;
