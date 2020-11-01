b91e2b5af0305bbe91bd61c004f7e81338b34914
Damian Kalka <kalka762@gmail.com>
2020-11-01 17:18:12 +0100

    createa and import GlobalStyle to layout

---
+++ src/GlobalStyles/GlobalStyles.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *:before,
+    *:after {
+        box-sizing: border-box;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        font-family: 'Montserrat';
+    }
+
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+    }
+
+    html {
+        font-size: 62.5%;
+    }
+
+    body {
+        font-size: 1.6rem;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button {
+        cursor: pointer;
+        padding: 0;
+    }
+
+    h1, h2, h3, h4, h5, h6, p {
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+export default GlobalStyle;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // import styled from 'styled-components';
 import propTypes from 'prop-types';
+import GlobalStyle from '../GlobalStyles/GlobalStyles';
 
 const MainLayout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <h1>Hello World!</h1>
       {children}
     </>
