e5d2235f474ca4d3ff23651bf5858f25d42e8a6b
Damian Kalka <kalka762@gmail.com>
2020-11-07 01:29:51 +0100

    add new dependencies and initialize gasby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,27 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path');
+const slugify = require('slugify');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const PostTemplate = path.resolve(`src/layouts/post.js`);
+  const result = await graphql(`
+    query queryCMSPage {
+      allDatoCmsPost {
+        nodes {
+          title
+          id
+        }
+      }
+    }
+  `);
+
+  result.data.allDatoCmsPost.nodes.forEach(post => {
+    createPage({
+      path: `posts/${slugify(post.title, { lower: true })}`,
+      component: PostTemplate,
+      context: {
+        postId: post.id,
+      },
+    });
+  });
+};
