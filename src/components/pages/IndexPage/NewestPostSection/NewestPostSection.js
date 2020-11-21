d91c05477b1e92df50b4ff0503ce6e28db39f48c
Damian Kalka <kalka762@gmail.com>
2020-11-21 16:25:28 +0100

    remove files from older version of newest post wrapper

--- src/components/pages/IndexPage/NewestPostSection/NewestPostSection.js
+++ src/components/pages/IndexPage/NewestPostSection/NewestPostSection.js
@@ -1,28 +1,10 @@
 import React from 'react';
-// import { graphql, useStaticQuery } from 'gatsby';
-import { graphql } from 'gatsby';
-// import NewPostElement from 'components/NewPostElement/NewPostElement';
 import PageLink from 'components/PageLink/PageLink';
 import Wires from 'assets/svg/wires.svg';
 import StyledNewestPostSection from './NewestPostSection.styled';
 import NewPostCarousel from '../../../NewPostCarousel/NewPostCarousel';
 
-export const query = graphql`
-  {
-    allDatoCmsPost(limit: 3) {
-      nodes {
-        shortDescription
-        title
-        date
-        id
-      }
-    }
-  }
-`;
-
 const NewestPostSection = () => {
-  // const data = useStaticQuery(query);
-
   return (
     <StyledNewestPostSection>
       <Wires className="newest-post__icon" />
@@ -31,12 +13,7 @@ const NewestPostSection = () => {
           <span style={{ color: '#61DAFB' }}>Najnowsze</span>
           <br /> posty
         </h2>
-        {/* <div className="newest-post__wrapper"> */}
-        {/* {data.allDatoCmsPost.nodes.map(post => (
-            <NewPostElement data={post} key={post.id} />
-          ))} */}
         <NewPostCarousel className="newest-post__wrapper" />
-        {/* </div> */}
       </div>
       <PageLink to="/posts">Zobacz wszystkie wpisy</PageLink>
     </StyledNewestPostSection>
