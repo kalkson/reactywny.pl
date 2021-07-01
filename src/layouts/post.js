1bffe83e5f048ec00bb106911293e1d70e385695
Damian Kalka <kalka762@gmail.com>
2021-07-02 00:09:49 +0200

    fix node error

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -1,24 +1,31 @@
+/* eslint-disable no-undef */
 import React from 'react';
-import { GatsbyImage } from 'gatsby-plugin-image';
 import propTypes from 'prop-types';
-import { Disqus } from 'gatsby-plugin-disqus';
 import { graphql, Link } from 'gatsby';
+
+import { GatsbyImage } from 'gatsby-plugin-image';
+import { Disqus } from 'gatsby-plugin-disqus';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import {
-  xonokai as materialDark,
-  solarizedlight,
+  darcula as darkTheme,
+  solarizedlight as lightTheme,
 } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import Headline from 'components/atoms/Headline/Headline';
+import SEO from 'seo/SEO';
+import HomeIcon from 'assets/svg/meta/home.svg';
+import PostPhoto from 'components/molecules/PostPhoto/PostPhoto';
+import NewsletterBar from 'components/organisms/NewsletterBar/NewsletterBar';
+import Post from 'components/atoms/Post/Post';
+
 import StyledPostLayout from './styled/post.styled';
-import SEO from '../seo/SEO';
-import HomeIcon from '../assets/svg/home.svg';
-import withNewsletter from '../hoc/withNewsletter';
-import PageHeader from '../components/organisms/PageHeader/PageHeader';
-import PageFooter from '../components/molecules/PageFooter/PageFooter';
-import Adnotation from '../components/molecules/Adnotation/Adnotation';
-import PostParagraph from '../components/molecules/PostParagraph/PostParagraph';
+
+const trimStringFromParentTag = (stringToReplace, tag = '') => {
+  return stringToReplace.replace(new RegExp(`^<${tag}>|</${tag}>$`, 'g'), '');
+};
 
 export const query = graphql`
-  query querySingleDatoCMSPost($id: String!) {
+  query querySingleDatoCMSPost($id: String!, $nextId: String!) {
     datoCmsPost(id: { eq: $id }) {
       id
       date
@@ -80,6 +87,23 @@ export const query = graphql`
         }
       }
     }
+    allDatoCmsPost(filter: { id: { eq: $nextId } }) {
+      nodes {
+        id
+        date
+        description
+        title
+        category
+        featuredImage {
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            placeholder: BLURRED
+            width: 300
+            forceBlurhash: true
+          )
+        }
+      }
+    }
   }
 `;
 
@@ -91,6 +115,30 @@ const PostLayout = ({ data }) => {
       }
     : null;
 
+  const months = [
+    'stycznia',
+    'lutego',
+    'marca',
+    'kwietnia',
+    'maja',
+    'czerwca',
+    'lipca',
+    'sierpnia',
+    'września',
+    'października',
+    'listopada',
+    'grudnia',
+  ];
+
+  const parsedDate = new Date(data.datoCmsPost.date);
+  const postDate = `${parsedDate.getDate()} ${
+    months[parsedDate.getMonth()]
+  } ${parsedDate.getFullYear()}`;
+
+  const previousPost = data.allDatoCmsPost.nodes[0]
+    ? data.allDatoCmsPost.nodes[0]
+    : null;
+
   if (data)
     return (
       <>
@@ -100,159 +148,247 @@ const PostLayout = ({ data }) => {
           post
           image={data.datoCmsPost.featuredImage.url}
         />
-        <PageHeader />
-        <StyledPostLayout>
-          <nav className="post__nav">
-            <Link className="post__nav__previous" to="/posts">
-              <HomeIcon /> Wpisy
-            </Link>
-            &nbsp; - &nbsp;
-            <span className="post__nav__current">{data.datoCmsPost.title}</span>
-          </nav>
-          <div className="post__top">
-            <time className="post__top__date">{data.datoCmsPost.date}</time>
-            <span className="post__top__category">
-              {data.datoCmsPost.category}
-            </span>
-          </div>
-          <h1 className="post__title">{data.datoCmsPost.title}</h1>
-          <GatsbyImage
-            image={data.datoCmsPost.featuredImage.gatsbyImageData}
-            className="post__featuredImage"
-          />
 
-          <span className="post__description">
-            {data.datoCmsPost.description}
-          </span>
-          {data.datoCmsPost.isTabled && (
-            <nav className="post__navHead">
-              <h4>Nawigacja</h4>
-              <ul className="post__navHead__list">
-                <br />
-                {data.datoCmsPost.postContent.map(item => {
+        <StyledPostLayout>
+          <header className="post__main-info-group">
+            <GatsbyImage
+              image={data.datoCmsPost.featuredImage.gatsbyImageData}
+              className="post__featuredImage"
+              alt=""
+              objectFit="cover"
+            />
+            <div className="container post__headline-wrapper">
+              <div className="post__headline-container">
+                <Headline className="post__headline">
+                  {data.datoCmsPost.title}
+                </Headline>
+                <div className="post__meta-info-group">
+                  <span aria-label="wpis utworzono: ">{postDate}</span>
+                  <span aria-label="napisano przez: ">Damian Kalka</span>
+                  <span aria-label="kategoria: " className="post__category">
+                    {data.datoCmsPost.category}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </header>
+
+          <div className="container post__grid">
+            <div className="post__content">
+              <nav className="post__nav">
+                <Link to="/" className="post__nav-link">
+                  <HomeIcon />
+                  Strona główna
+                </Link>
+                &nbsp; - &nbsp;
+                <Link to="/posts" className="post__nav-link">
+                  Wpisy
+                </Link>
+                &nbsp; - &nbsp;
+                <span>{data.datoCmsPost.title}</span>
+              </nav>
+
+              <p className="post__description">
+                {data.datoCmsPost.description}
+              </p>
+
+              {data.datoCmsPost.isTabled && (
+                <nav className="post__headings-navigation">
+                  <h2>Nawigacja</h2>
+                  <ul className="post__headings-list">
+                    {data.datoCmsPost.postContent.map(item => {
+                      const itemKey = Object.keys(item)[1];
+
+                      if (itemKey === 'headingContent')
+                        return (
+                          <li className="post__headings-item" key={item.id}>
+                            <a href={`#${item.id}`}>{item.headingContent}</a>
+                          </li>
+                        );
+
+                      return null;
+                    })}
+                  </ul>
+                </nav>
+              )}
+
+              {data.datoCmsPost.postContent &&
+                data.datoCmsPost.postContent.map(item => {
                   const itemKey = Object.keys(item)[1];
 
-                  if (itemKey === 'headingContent')
-                    return (
-                      <li className="post__navHead__list__item" key={item.id}>
-                        <a href={`#${item.id}`}>{item.headingContent}</a>
-                      </li>
-                    );
+                  switch (itemKey) {
+                    case 'paragraphContent':
+                      return (
+                        <p
+                          className="post__paragraph"
+                          key={item.id}
+                          // eslint-disable-next-line react/no-danger
+                          dangerouslySetInnerHTML={{
+                            __html: trimStringFromParentTag(
+                              item.paragraphContent
+                            ),
+                          }}
+                        />
+                      );
 
-                  return null;
+                    // -----------------------------
+
+                    case 'imageData':
+                      return <PostPhoto key={item.id} {...item} />;
+
+                    // -----------------------------
+
+                    case 'headingContent':
+                      return (
+                        <h2
+                          className="post__heading"
+                          key={item.id}
+                          id={item.id}
+                        >
+                          {item.headingContent}
+                        </h2>
+                      );
+
+                    // -----------------------------
+
+                    case 'syntaxContent':
+                      return (
+                        <div className="post__code-container">
+                          <div
+                            className="post__language-note"
+                            role="presentation"
+                          >
+                            {item.language}
+                          </div>
+                          <SyntaxHighlighter
+                            language={item.language}
+                            style={darkTheme}
+                            showLineNumbers={false}
+                            className="post__code"
+                            key={item.id}
+                            translate="no"
+                          >
+                            {item.syntaxContent}
+                          </SyntaxHighlighter>
+                        </div>
+                      );
+
+                    // -----------------------------
+
+                    case 'cliContent':
+                      return (
+                        <div className="post__cli-container">
+                          <div role="presentation" className="post__cli-note">
+                            cli
+                          </div>
+                          <SyntaxHighlighter
+                            style={lightTheme}
+                            className="post__cli"
+                            showLineNumbers={false}
+                            key={item.id}
+                          >
+                            {item.cliContent}
+                          </SyntaxHighlighter>
+                        </div>
+                      );
+
+                    // -----------------------------
+
+                    case 'videoLink':
+                      return (
+                        <iframe
+                          key={item.videolink.title}
+                          className="post__video"
+                          title={item.videoLink.title}
+                          src={item.videoLink.url.replace('watch?v=', 'embed/')}
+                          frameBorder="0"
+                          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                          allowFullScreen
+                        />
+                      );
+
+                    // -----------------------------
+
+                    case 'quoteContent':
+                      return (
+                        <q
+                          className="post__quote"
+                          key={item.id}
+                          content={item.quoteContent}
+                          // eslint-disable-next-line react/no-danger
+                          dangerouslySetInnerHTML={{
+                            __html: trimStringFromParentTag(
+                              item.quoteContent,
+                              'p'
+                            ),
+                          }}
+                        />
+                      );
+
+                    // -----------------------------
+
+                    default:
+                      return null;
+                  }
                 })}
-              </ul>
-            </nav>
-          )}
-          {data.datoCmsPost.postContent &&
-            data.datoCmsPost.postContent.map(item => {
-              const itemKey = Object.keys(item)[1];
 
-              switch (itemKey) {
-                case 'paragraphContent':
-                  return (
-                    <PostParagraph
-                      className="post__paragraph"
-                      key={item.id}
-                      content={item.paragraphContent}
-                    />
-                  );
-                case 'imageData':
-                  return (
-                    <div className="post__photo" key={item.id}>
-                      {/* <Image
-                        className="post__photo__image"
-                        fluid={item.imageData.fluid}
-                        style={{ width: '100%' }}
-                      /> */}
-                      <GatsbyImage
-                        className="post__photo__image"
-                        image={item.imageData.gatsbyImageData}
-                        // fluid={item.imageData.fluid}
-                        // style={{ width: '100%' }}
-                      />
-                      <span className="post__photo__sign">
-                        {item.postImageSign}
-                      </span>
-                    </div>
-                  );
-                case 'headingContent':
-                  return (
-                    <h2 className="post__heading" key={item.id} id={item.id}>
-                      {item.headingContent}
-                    </h2>
-                  );
-                case 'syntaxContent':
-                  return (
-                    <SyntaxHighlighter
-                      language={item.language}
-                      style={materialDark}
-                      showLineNumbers
-                      className="post__syntax"
-                      key={item.id}
-                    >
-                      {item.syntaxContent}
-                    </SyntaxHighlighter>
-                  );
-                case 'cliContent':
-                  return (
-                    <SyntaxHighlighter
-                      style={solarizedlight}
-                      className="post__cli"
-                      key={item.id}
-                    >
-                      {item.cliContent}
-                    </SyntaxHighlighter>
-                  );
-                case 'videoLink':
-                  return (
-                    <iframe
-                      key={item.videolink.title}
-                      className="post__video"
-                      title={item.videoLink.title}
-                      src={item.videoLink.url.replace('watch?v=', 'embed/')}
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    />
-                  );
-                case 'quoteContent':
-                  return (
-                    <PostParagraph
-                      className="post__quote"
-                      key={item.id}
-                      content={item.quoteContent}
-                    />
-                  );
-
-                default:
-                  return null;
-              }
-            })}
-          {data.datoCmsPost.postSource && (
-            <div className="post__source">
-              <h4 className="post__source__heading">Źródła</h4>
-              <ul className="post__source__list">
-                {JSON.parse(data.datoCmsPost.postSource).sources.map(source => {
-                  return (
-                    <li key={source} className="post__source__list__item">
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={source}
-                      >
-                        {source}
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
+              {data.datoCmsPost.postSource && (
+                <div className="post__sources">
+                  <h3 className="post__sources-heading">Źródła</h3>
+                  <ul className="post__sources-list">
+                    {JSON.parse(data.datoCmsPost.postSource).sources.map(
+                      source => {
+                        return (
+                          <li key={source} className="post__sources-item">
+                            <a
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              href={source}
+                            >
+                              {source}
+                            </a>
+                          </li>
+                        );
+                      }
+                    )}
+                  </ul>
+                </div>
+              )}
             </div>
-          )}
-          <Adnotation />
-          <Disqus config={disqusConfig} className="post__disquis" />
+            {previousPost && (
+              <aside>
+                <h3>Przeczytaj także</h3>
+                <Post
+                  date={previousPost.date}
+                  category={previousPost.category}
+                  title={previousPost.title}
+                  description={previousPost.description}
+                  featuredImage={previousPost.featuredImage}
+                  className="post__random-post"
+                />
+              </aside>
+            )}
+          </div>
+
+          <footer>
+            <div className="container">
+              <NewsletterBar>
+                <>
+                  Spodobał ci się post? Dołącz do newslettera, aby być na
+                  bieżąco z każdym nowym wpisem na blogu. O innych korzyściach z
+                  mojego newslettera dowiesz się{' '}
+                  <Link
+                    aria-label="odnośnik do opisu newslettera"
+                    to="/newsletter"
+                  >
+                    tutaj.
+                  </Link>
+                </>
+              </NewsletterBar>
+              <Disqus config={disqusConfig} className="post__disquis" />
+            </div>
+          </footer>
         </StyledPostLayout>
-        <PageFooter />
       </>
     );
 
@@ -281,11 +417,15 @@ PostLayout.propTypes = {
         gatsbyImageData: propTypes.shape(),
       }).isRequired,
     }).isRequired,
+    allDatoCmsPost: propTypes.shape({
+      nodes: propTypes.arrayOf(propTypes.shape),
+    }),
   }),
 };
 
 PostLayout.defaultProps = {
   data: {
+    allDatoCmsPost: null,
     datoCmsPost: {
       featuredImage: {
         fluid: null,
@@ -297,4 +437,4 @@ PostLayout.defaultProps = {
   },
 };
 
-export default withNewsletter(PostLayout);
+export default PostLayout;
